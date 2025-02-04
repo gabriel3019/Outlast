db7c81e17d7dac9e77c1ba7fe933994a6e435e88
Celia115 <celianmolin@gmail.com>
2025-02-04 23:08:48 +0100

    enemigos actualizado

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
 
     void OnTriggerExit2D(Collider2D collision)
     {
@@ -32,7 +39,10 @@ public class Reposition : MonoBehaviour
                 }
                 break;
             case "Enemy":
-
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range( - 3f, 3f), 0f));
+                }
                 break;
         }
 
